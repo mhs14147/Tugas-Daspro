ecf014528aed7d3de9667f6ea737bffd2d8d8c94
mhs14147 <112869021+mhs14147@users.noreply.github.com>
2022-09-07 00:56:47 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "NAMA :FAJAR ADJI WICAKSANA" << endl;
+    cout << "NIM :A11.2022.14147" << endl;
+    cout << "ALAMAT :DEMAK" << endl;
+    cout << "ASAL :SMAN MRANGGEN" << endl;
+    return 0;
+
+}
