6fdebf0a5b025849735a014c61165daa4d344050
Fajar Adji Wicaksana <112869021+mhs14147@users.noreply.github.com>
2022-09-12 20:46:22 +0700

    Add files via upload

---
+++ PERTEMUAN DASPRO 3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "FAW" << endl;
+    printf ("umur 19");
+
+    int x = 19;
+    float t = 171.0;
+    char b = 'L';
+
+    cout << "umur saya" << x;
+    cout << "tinggi badan" << t;
+    cout << "ukuran baju" << b;
+
+    return 0;
+}
